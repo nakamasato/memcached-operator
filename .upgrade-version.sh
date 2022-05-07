7f30e5a573a25b783eaee257f5a7e5a4b3e7a838
Naka Masato <masatonaka1989@gmail.com>
2022-05-07 11:41:32 +0900

    upgrade-version until 4.2

--- .upgrade-version.sh
+++ .upgrade-version.sh
@@ -132,3 +132,90 @@ make fmt
 git add .
 pre-commit run -a || true
 git commit -am "4.1. Fetch Memcached instance."
+
+## 4.2 Check if the deployment already exists, and create one if not exists.
+gsed -i '/^import/a "k8s.io/apimachinery/pkg/types"' $MEMCACHED_CONTROLLER_GO_FILE
+gsed -i '/^import/a appsv1 "k8s.io/api/apps/v1"' $MEMCACHED_CONTROLLER_GO_FILE
+gsed -i '/^import/a corev1 "k8s.io/api/core/v1"' $MEMCACHED_CONTROLLER_GO_FILE
+gsed -i '/^import/a metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"' $MEMCACHED_CONTROLLER_GO_FILE
+
+cat << EOF > tmpfile
+
+// 2. Check if the deployment already exists, if not create a new one
+found := &appsv1.Deployment{}
+err = r.Get(ctx, types.NamespacedName{Name: memcached.Name, Namespace: memcached.Namespace}, found)
+if err != nil && errors.IsNotFound(err) {
+        // Define a new deployment
+        dep := r.deploymentForMemcached(memcached)
+        log.Info("2. Check if the deployment already exists, if not create a new one. Creating a new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+        err = r.Create(ctx, dep)
+        if err != nil {
+                log.Error(err, "2. Check if the deployment already exists, if not create a new one. Failed to create new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+                return ctrl.Result{}, err
+        }
+        // Deployment created successfully - return and requeue
+        return ctrl.Result{Requeue: true}, nil
+} else if err != nil {
+        log.Error(err, "2. Check if the deployment already exists, if not create a new one. Failed to get Deployment")
+        return ctrl.Result{}, err
+}
+EOF
+# Add the contents before the last return in Reconcile function.
+gsed -i $'/^\treturn ctrl.Result{}, nil/{e cat tmpfile\n}' $MEMCACHED_CONTROLLER_GO_FILE
+
+cat << EOF > tmpfile
+
+// deploymentForMemcached returns a memcached Deployment object
+func (r *MemcachedReconciler) deploymentForMemcached(m *cachev1alpha1.Memcached) *appsv1.Deployment {
+    ls := labelsForMemcached(m.Name)
+    replicas := m.Spec.Size
+
+    dep := &appsv1.Deployment{
+            ObjectMeta: metav1.ObjectMeta{
+                    Name:      m.Name,
+                    Namespace: m.Namespace,
+            },
+            Spec: appsv1.DeploymentSpec{
+                    Replicas: &replicas,
+                    Selector: &metav1.LabelSelector{
+                            MatchLabels: ls,
+                    },
+                    Template: corev1.PodTemplateSpec{
+                            ObjectMeta: metav1.ObjectMeta{
+                                    Labels: ls,
+                            },
+                            Spec: corev1.PodSpec{
+                                    Containers: []corev1.Container{{
+                                            Image:   "memcached:1.4.36-alpine",
+                                            Name:    "memcached",
+                                            Command: []string{"memcached", "-m=64", "-o", "modern", "-v"},
+                                            Ports: []corev1.ContainerPort{{
+                                                    ContainerPort: 11211,
+                                                    Name:          "memcached",
+                                            }},
+                                    }},
+                            },
+                    },
+            },
+    }
+    // Set Memcached instance as the owner and controller
+    ctrl.SetControllerReference(m, dep, r.Scheme)
+    return dep
+}
+
+// labelsForMemcached returns the labels for selecting the resources
+// belonging to the given memcached CR name.
+func labelsForMemcached(name string) map[string]string {
+    return map[string]string{"app": "memcached", "memcached_cr": name}
+}
+EOF
+cat tmpfile >> $MEMCACHED_CONTROLLER_GO_FILE
+rm tmpfile
+
+gsed -i '/kubebuilder:rbac:groups=cache.example.com,resources=memcacheds\/finalizers/a \/\/+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete' $MEMCACHED_CONTROLLER_GO_FILE
+gsed -i '/For(&cachev1alpha1.Memcached{})/a Owns(&appsv1.Deployment{}).' $MEMCACHED_CONTROLLER_GO_FILE
+make fmt manifests
+
+git add .
+pre-commit run -a || true
+git commit -am "4.2 Check if the deployment already exists, and create one if not exists."
