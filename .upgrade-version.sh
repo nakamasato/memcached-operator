ee2ad4d425797a0bd2f88949236faeecf7d09247
Masato Naka <masatonaka1989@gmail.com>
2022-10-17 08:56:47 +0900

    update .upgrade-version.sh

--- .upgrade-version.sh
+++ .upgrade-version.sh
@@ -48,8 +48,8 @@ else
 fi
 
 echo "======== CLEAN UP COMPLETED ==========="
-gsed -i "s/go-version:.*/go-version: $GO_VERSION/g" .github/workflows/test.yml
-gsed -i "s/go-version:.*/go_version: $GO_VERSION/g" .github/workflows/reviewdog.yml
+gsed -i "s/go-version:.*/go-version: ${GO_VERSION/go/}/g" .github/workflows/test.yml
+gsed -i "s/go-version:.*/go_version: ${GO_VERSION/go/}/g" .github/workflows/reviewdog.yml
 
 # 1. Init a project
 echo "======== INIT PROJECT ==========="
