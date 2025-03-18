09ef61e1a7bfb96d3c6199202509dced1718569b
Masato Naka <masatonaka1989@gmail.com>
2025-03-18 13:18:14 +0900

    5. Write controller test

--- internal/controller/memcached_controller_test.go
+++ internal/controller/memcached_controller_test.go
@@ -1,84 +1,230 @@
-/*
-Copyright 2025.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package controller
 
 import (
 	"context"
-
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
-	"k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/types"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
-
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"fmt"
+	"time"
 
 	cachev1alpha1 "github.com/example/memcached-operator/api/v1alpha1"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	appsv1 "k8s.io/api/apps/v1"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-var _ = Describe("Memcached Controller", func() {
-	Context("When reconciling a resource", func() {
-		const resourceName = "test-resource"
+const (
+	memcachedApiVersion = "cache.example.com/v1alphav1"
+	memcachedKind       = "Memcached"
+	memcachedName       = "sample"
+	memcachedNamespace  = "default"
+	memcachedStartSize  = int32(3)
+	memcachedUpdateSize = int32(10)
+	timeout             = time.Second * 10
+	interval            = time.Millisecond * 250
+)
 
-		ctx := context.Background()
+var _ = Describe("Memcached controller", func() {
 
-		typeNamespacedName := types.NamespacedName{
-			Name:      resourceName,
-			Namespace: "default", // TODO(user):Modify as needed
-		}
-		memcached := &cachev1alpha1.Memcached{}
+	lookUpKey := types.NamespacedName{Name: memcachedName, Namespace: memcachedNamespace}
 
+	AfterEach(func() {
+		// Delete Memcached
+		deleteMemcached(ctx, lookUpKey)
+		// Delete all Pods
+		deleteAllPods(ctx)
+	})
+
+	Context("When creating Memcached", func() {
 		BeforeEach(func() {
-			By("creating the custom resource for the Kind Memcached")
-			err := k8sClient.Get(ctx, typeNamespacedName, memcached)
-			if err != nil && errors.IsNotFound(err) {
-				resource := &cachev1alpha1.Memcached{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      resourceName,
-						Namespace: "default",
-					},
-					// TODO(user): Specify other spec details if needed.
-				}
-				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
-			}
+			// Create Memcached
+			createMemcached(ctx, memcachedStartSize)
 		})
-
-		AfterEach(func() {
-			// TODO(user): Cleanup logic after each test, like removing the resource instance.
-			resource := &cachev1alpha1.Memcached{}
-			err := k8sClient.Get(ctx, typeNamespacedName, resource)
-			Expect(err).NotTo(HaveOccurred())
-
-			By("Cleanup the specific resource instance Memcached")
-			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
+		It("Should create Deployment with the specified size and memcached image", func() {
+			// Deployment is created
+			deployment := &appsv1.Deployment{}
+			Eventually(func() error {
+				return k8sClient.Get(ctx, lookUpKey, deployment)
+			}, timeout, interval).Should(Succeed())
+			Expect(*deployment.Spec.Replicas).Should(Equal(memcachedStartSize))
+			Expect(deployment.Spec.Template.Spec.Containers[0].Image).Should(Equal("memcached:1.4.36-alpine"))
+			// https://github.com/kubernetes-sigs/controller-runtime/blob/master/pkg/controller/controllerutil/controllerutil_test.go
+			Expect(deployment.OwnerReferences).ShouldNot(BeEmpty())
 		})
-		It("should successfully reconcile the resource", func() {
-			By("Reconciling the created resource")
-			controllerReconciler := &MemcachedReconciler{
-				Client: k8sClient,
-				Scheme: k8sClient.Scheme(),
-			}
+		It("Should have pods name in Memcached Node", func() {
+			checkIfDeploymentExists(ctx, lookUpKey)
 
-			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
-				NamespacedName: typeNamespacedName,
-			})
-			Expect(err).NotTo(HaveOccurred())
-			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
-			// Example: If you expect a certain status condition after reconciliation, verify it here.
+			By("By creating Pods with labels")
+			podNames := createPods(ctx, int(memcachedStartSize))
+
+			updateMemcacheSize(ctx, lookUpKey, memcachedUpdateSize) // just to trigger reconcile
+
+			checkMemcachedStatusNodes(ctx, lookUpKey, podNames)
 		})
 	})
+
+	Context("When updating Memcached", func() {
+		BeforeEach(func() {
+			// Create Memcached
+			createMemcached(ctx, memcachedStartSize)
+			// Deployment is ready
+			checkDeploymentReplicas(ctx, lookUpKey, memcachedStartSize)
+		})
+
+		It("Should update Deployment replicas", func() {
+			By("Changing Memcached size")
+			updateMemcacheSize(ctx, lookUpKey, memcachedUpdateSize)
+
+			checkDeploymentReplicas(ctx, lookUpKey, memcachedUpdateSize)
+		})
+
+		It("Should update the Memcached status with the pod names", func() {
+			By("Changing Memcached size")
+			updateMemcacheSize(ctx, lookUpKey, memcachedUpdateSize)
+
+			podNames := createPods(ctx, int(memcachedUpdateSize))
+			checkMemcachedStatusNodes(ctx, lookUpKey, podNames)
+		})
+	})
+	Context("When changing Deployment", func() {
+		BeforeEach(func() {
+			// Create Memcached
+			createMemcached(ctx, memcachedStartSize)
+			// Deployment is ready
+			checkDeploymentReplicas(ctx, lookUpKey, memcachedStartSize)
+		})
+
+		It("Should check if the deployment already exists, if not create a new one", func() {
+			By("Deleting Deployment")
+			deployment := &appsv1.Deployment{}
+			Expect(k8sClient.Get(ctx, lookUpKey, deployment)).Should(Succeed())
+			Expect(k8sClient.Delete(ctx, deployment)).Should(Succeed())
+
+			// Deployment will be recreated by the controller
+			checkIfDeploymentExists(ctx, lookUpKey)
+		})
+
+		It("Should ensure the deployment size is the same as the spec", func() {
+			By("Changing Deployment replicas")
+			deployment := &appsv1.Deployment{}
+			Expect(k8sClient.Get(ctx, lookUpKey, deployment)).Should(Succeed())
+			*deployment.Spec.Replicas = 0
+			Expect(k8sClient.Update(ctx, deployment)).Should(Succeed())
+
+			// replicas will be updated back to the original one by the controller
+			checkDeploymentReplicas(ctx, lookUpKey, memcachedStartSize)
+		})
+	})
+
+	// Deployment is expected to be deleted when Memcached is deleted.
+	// As it's garbage collector's responsibility, which is not part of envtest, we don't test it here.
 })
+
+func checkIfDeploymentExists(ctx context.Context, lookUpKey types.NamespacedName) {
+	deployment := &appsv1.Deployment{}
+	Eventually(func() error {
+		return k8sClient.Get(ctx, lookUpKey, deployment)
+	}, timeout, interval).Should(Succeed())
+}
+
+func checkDeploymentReplicas(ctx context.Context, lookUpKey types.NamespacedName, expectedSize int32) {
+	Eventually(func() (int32, error) {
+		deployment := &appsv1.Deployment{}
+		err := k8sClient.Get(ctx, lookUpKey, deployment)
+		if err != nil {
+			return int32(0), err
+		}
+		return *deployment.Spec.Replicas, nil
+	}, timeout, interval).Should(Equal(expectedSize))
+}
+
+func newMemcached(size int32) *cachev1alpha1.Memcached {
+	return &cachev1alpha1.Memcached{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: memcachedApiVersion,
+			Kind:       memcachedKind,
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      memcachedName,
+			Namespace: memcachedNamespace,
+		},
+		Spec: cachev1alpha1.MemcachedSpec{
+			Size: size,
+		},
+	}
+}
+
+func createMemcached(ctx context.Context, size int32) {
+	memcached := newMemcached(size)
+	Expect(k8sClient.Create(ctx, memcached)).Should(Succeed())
+}
+
+func updateMemcacheSize(ctx context.Context, lookUpKey types.NamespacedName, size int32) {
+	memcached := &cachev1alpha1.Memcached{}
+	Expect(k8sClient.Get(ctx, lookUpKey, memcached)).Should(Succeed())
+	memcached.Spec.Size = size
+	Expect(k8sClient.Update(ctx, memcached)).Should(Succeed())
+}
+
+func deleteMemcached(ctx context.Context, lookUpKey types.NamespacedName) {
+	memcached := &cachev1alpha1.Memcached{}
+	Expect(k8sClient.Get(ctx, lookUpKey, memcached)).Should(Succeed())
+	Expect(k8sClient.Delete(ctx, memcached)).Should(Succeed())
+}
+
+func checkMemcachedStatusNodes(ctx context.Context, lookUpKey types.NamespacedName, podNames []string) {
+	memcached := &cachev1alpha1.Memcached{}
+	Eventually(func() ([]string, error) {
+		err := k8sClient.Get(ctx, lookUpKey, memcached)
+		if err != nil {
+			return nil, err
+		}
+		return memcached.Status.Nodes, nil
+	}, timeout, interval).Should(ConsistOf(podNames))
+}
+
+func createPods(ctx context.Context, num int) []string {
+	podNames := []string{}
+	for i := 0; i < num; i++ {
+		podName := fmt.Sprintf("pod-%d", i)
+		podNames = append(podNames, podName)
+		pod := newPod(podName)
+		Expect(k8sClient.Create(ctx, pod)).Should(Succeed())
+	}
+	return podNames
+}
+
+func deleteAllPods(ctx context.Context) {
+	err := k8sClient.DeleteAllOf(ctx, &v1.Pod{}, client.InNamespace(memcachedNamespace))
+	Expect(err).NotTo(HaveOccurred())
+}
+
+func newPod(name string) *v1.Pod {
+	return &v1.Pod{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Pod",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: memcachedNamespace,
+			Labels: map[string]string{
+				"app":          "memcached",
+				"memcached_cr": memcachedName,
+			},
+		},
+		Spec: v1.PodSpec{
+			Containers: []v1.Container{
+				{
+					Name:  "memcached",
+					Image: "memcached",
+				},
+			},
+		},
+		Status: v1.PodStatus{},
+	}
+}
